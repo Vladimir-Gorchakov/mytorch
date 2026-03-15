955b36307a33637b18c9c3d93e2bf4c50a2d681e
Vladimi <tdfsosat@gmail.com>
2026-03-16 00:32:59 +0300

    formated with clang-format

--- utils.hpp
+++ utils.hpp
@@ -2,16 +2,16 @@
 #include <sstream>
 #include <vector>
 
-namespace  torch::utils {
-    template<typename T>
-    std::string vec_to_string(const std::vector<T>& v) {
-        std::ostringstream oss;
-        oss << "[";
-        for (size_t i = 0; i < v.size(); ++i) {
-            if (i) oss << ", ";
-            oss << v[i];
-        }
-        oss << "]";
-        return oss.str();
-    }
+namespace torch::utils {
+template <typename T> std::string vec_to_string(const std::vector<T> &v) {
+  std::ostringstream oss;
+  oss << "[";
+  for (size_t i = 0; i < v.size(); ++i) {
+    if (i)
+      oss << ", ";
+    oss << v[i];
+  }
+  oss << "]";
+  return oss.str();
 }
+} // namespace torch::utils
