18d28843c6f30b90006c0035c920461f07cfce4c
Vladimi <tdfsosat@gmail.com>
2026-03-16 21:37:37 +0300

    fixed const logic, added separe handling of input and output

--- tensor_iterator.hpp
+++ tensor_iterator.hpp
@@ -13,8 +13,8 @@ class shape_exception;
 }
 
 namespace torch::iterator {
-using shape_t = std::vector<size_t>;
 using size_t = std::size_t;
+using shape_t = std::vector<size_t>;
 
 template <typename T>
 class Operand {
@@ -23,7 +23,9 @@ class Operand {
 
     shape_t strides_;
 
-    Operand(T* data_ptr, shape_t strides) : data_ptr_(data_ptr), strides_(strides) {}
+    Operand(T* data_ptr, shape_t strides)
+        : data_ptr_(data_ptr),
+          strides_(strides) {}
 };
 
 template <typename T>
@@ -31,39 +33,48 @@ class TensorIterator {
   private:
     shape_t shape_;
 
-    std::vector<Operand<T>> inputs_;
+    std::vector<Operand<const T>> inputs_;
     std::vector<Operand<T>> outputs_;
 
   public:
-    TensorIterator(const std::vector<Tensor<T>*>& inputs, const std::vector<Tensor<T>*>& outputs);
+    TensorIterator(const std::vector<const Tensor<T>*>& inputs,
+                   const std::vector<Tensor<T>*>& outputs);
 
     size_t numel() const;
 
-    shape_t broadcast_shape(const std::vector<Tensor<T>*>& inputs);
+    static shape_t broadcast_shape(const std::vector<const Tensor<T>*>& inputs);
 
     shape_t compute_strides(shape_t strides, shape_t shape) const;
 
     shape_t shape();
 
-    void _move_ptrs(std::vector<T*>& ptrs, std::vector<shape_t*>& strides, int step, size_t dim);
-    void run(std::function<void(std::span<T*>)> kernel);
+    void _move_ptrs(std::vector<T*>& ptrs, std::vector<shape_t*>& strides,
+                    int step, size_t dim);
+
+    void _move_ptrs(std::vector<const T*>& ptrs, std::vector<shape_t*>& strides,
+                    int step, size_t dim);
+
+    void run(std::function<void(std::span<const T*>, std::span<T*>)> kernel);
 };
 
 template <typename T>
-TensorIterator<T>::TensorIterator(const std::vector<Tensor<T>*>& inputs,
+TensorIterator<T>::TensorIterator(const std::vector<const Tensor<T>*>& inputs,
                                   const std::vector<Tensor<T>*>& outputs) {
     shape_ = broadcast_shape(inputs);
 
     for (auto tensor : inputs) {
         inputs_.push_back(
-            Operand(tensor->data_ptr(), compute_strides(tensor->strides(), tensor->shape())));
+            Operand(tensor->data_ptr(),
+                    compute_strides(tensor->strides(), tensor->shape())));
     }
 
     for (auto tensor : outputs) {
         if (!(shape_ == tensor->shape())) {
             throw exception::shape_exception{
-                std::format("Shape exception: output tensor shape must be {} but got {}",
-                            utils::vec_to_string(shape_), utils::vec_to_string(tensor->shape()))};
+                std::format("Shape exception: output tensor shape must be {} "
+                            "but got {}",
+                            utils::vec_to_string(shape_),
+                            utils::vec_to_string(tensor->shape()))};
         }
 
         outputs_.push_back(Operand(tensor->data_ptr(), tensor->strides()));
@@ -72,11 +83,13 @@ TensorIterator<T>::TensorIterator(const std::vector<Tensor<T>*>& inputs,
 
 template <typename T>
 size_t TensorIterator<T>::numel() const {
-    return std::accumulate(shape_.begin(), shape_.end(), 1u, std::multiplies<size_t>());
+    return std::accumulate(shape_.begin(), shape_.end(), 1u,
+                           std::multiplies<size_t>());
 }
 
 template <typename T>
-shape_t TensorIterator<T>::broadcast_shape(const std::vector<Tensor<T>*>& inputs) {
+shape_t TensorIterator<T>::broadcast_shape(
+    const std::vector<const Tensor<T>*>& inputs) {
     size_t shape_size = 0;
     for (auto tensor : inputs) {
         shape_size = std::max(shape_size, tensor->shape().size());
@@ -94,15 +107,17 @@ shape_t TensorIterator<T>::broadcast_shape(const std::vector<Tensor<T>*>& inputs
             size_t t_i = tensor->shape().size() - i;
             size_t s_i = shape.size() - i;
 
-            if (shape[s_i] != 1 && tensor->shape()[t_i] != 1 && shape[s_i] != tensor->shape()[t_i])
-                [[unlikely]] {
+            if (shape[s_i] != 1 && tensor->shape()[t_i] != 1 &&
+                shape[s_i] != tensor->shape()[t_i]) [[unlikely]] {
                 throw exception::shape_exception{
-                    std::format("Shape exception: output tensor shape at index {} "
+                    std::format("Shape exception: output tensor shape at index "
+                                "{} "
                                 "shuld be the same but got {} and {}",
                                 i, shape[s_i], tensor->shape()[t_i])};
             }
 
-            shape[s_i] = (tensor->shape()[t_i] == 1) ? shape[s_i] : tensor->shape()[t_i];
+            shape[s_i] =
+                (tensor->shape()[t_i] == 1) ? shape[s_i] : tensor->shape()[t_i];
         }
     }
 
@@ -110,17 +125,22 @@ shape_t TensorIterator<T>::broadcast_shape(const std::vector<Tensor<T>*>& inputs
 }
 
 template <typename T>
-shape_t TensorIterator<T>::compute_strides(shape_t strides, shape_t shape) const {
+shape_t TensorIterator<T>::compute_strides(shape_t strides,
+                                           shape_t shape) const {
     shape_t new_strides(shape_.size());
 
     for (size_t i = 0; i < shape_.size(); ++i) {
-        if (i < shape_.size() - strides.size()) { // Гарантированно strides.size() <= shape_.size()
+        if (i <
+            shape_.size() -
+                strides
+                    .size()) { // Гарантированно strides.size() <= shape_.size()
             continue;
         }
 
         size_t corrected_idx = i - (shape_.size() - strides.size());
 
-        new_strides[i] = shape[corrected_idx] != 1u ? strides[corrected_idx] : 0;
+        new_strides[i] =
+            shape[corrected_idx] != 1u ? strides[corrected_idx] : 0;
     }
 
     return new_strides;
@@ -132,7 +152,8 @@ shape_t TensorIterator<T>::shape() {
 }
 
 template <typename T>
-void TensorIterator<T>::_move_ptrs(std::vector<T*>& ptrs, std::vector<shape_t*>& strides, int step,
+void TensorIterator<T>::_move_ptrs(std::vector<const T*>& ptrs,
+                                   std::vector<shape_t*>& strides, int step,
                                    size_t dim) {
     for (size_t i = 0; i < ptrs.size(); ++i) {
         ptrs[i] += step * (int)(*strides[i])[dim];
@@ -140,18 +161,31 @@ void TensorIterator<T>::_move_ptrs(std::vector<T*>& ptrs, std::vector<shape_t*>&
 }
 
 template <typename T>
-void TensorIterator<T>::run(std::function<void(std::span<T*>)> kernel) {
-    std::vector<T*> ptrs{};
-    std::vector<shape_t*> strides{};
+void TensorIterator<T>::_move_ptrs(std::vector<T*>& ptrs,
+                                   std::vector<shape_t*>& strides, int step,
+                                   size_t dim) {
+    for (size_t i = 0; i < ptrs.size(); ++i) {
+        ptrs[i] += step * (int)(*strides[i])[dim];
+    }
+}
+
+template <typename T>
+void TensorIterator<T>::run(
+    std::function<void(std::span<const T*>, std::span<T*>)> kernel) {
+    std::vector<const T*> ptrs_input{};
+    std::vector<shape_t*> strides_input{};
+
+    std::vector<T*> ptrs_output{};
+    std::vector<shape_t*> strides_output{};
 
     for (auto& operand : inputs_) {
-        ptrs.push_back(operand.data_ptr_);
-        strides.push_back(&operand.strides_);
+        ptrs_input.push_back(operand.data_ptr_);
+        strides_input.push_back(&operand.strides_);
     }
 
     for (auto& operand : outputs_) {
-        ptrs.push_back(operand.data_ptr_);
-        strides.push_back(&operand.strides_);
+        ptrs_output.push_back(operand.data_ptr_);
+        strides_output.push_back(&operand.strides_);
     }
 
     size_t index = 0;
@@ -165,25 +199,27 @@ void TensorIterator<T>::run(std::function<void(std::span<T*>)> kernel) {
 
             if (index % shape_[j] == 0) {
                 index /= shape_[j];
-                _move_ptrs(ptrs, strides, -shape_[j] + 1, j);
+                _move_ptrs(ptrs_input, strides_input, -(int)shape_[j] + 1, j);
+                _move_ptrs(ptrs_output, strides_output, -(int)shape_[j] + 1, j);
             } else {
-                _move_ptrs(ptrs, strides, 1, j);
+                _move_ptrs(ptrs_input, strides_input, 1, j);
+                _move_ptrs(ptrs_output, strides_output, 1, j);
                 break;
             }
         }
 
-        kernel(std::span<T*>(ptrs));
+        kernel(std::span<const T*>(ptrs_input), std::span<T*>(ptrs_output));
     }
 }
 
 template <typename T>
 class TensorIteratorConfig {
   private:
-    std::vector<Tensor<T>*> inputs_;
+    std::vector<const Tensor<T>*> inputs_;
     std::vector<Tensor<T>*> outputs_;
 
   public:
-    TensorIteratorConfig& add_input(Tensor<T>& tensor) {
+    TensorIteratorConfig& add_input(const Tensor<T>& tensor) {
         inputs_.push_back(&tensor);
         return *this;
     }
@@ -193,7 +229,16 @@ class TensorIteratorConfig {
         return *this;
     }
 
-    TensorIterator<T> build() { return TensorIterator<T>{&inputs_, &outputs_}; }
+    TensorIterator<T> build() { // требует улучшения, пока не знаю как для
+                                // общего случая должны выглядить шейпы
+
+        if (outputs_[0]->empty()) {
+            shape_t shape = TensorIterator<T>::broadcast_shape(inputs_);
+            *outputs_[0] = Tensor<T>{shape};
+        }
+
+        return TensorIterator<T>{inputs_, outputs_};
+    }
 };
 
 } // namespace torch::iterator
