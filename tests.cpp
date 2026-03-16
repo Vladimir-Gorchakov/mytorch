2346401a1379c81b488e04f115d7ff44c87d4089
Vladimi <tdfsosat@gmail.com>
2026-03-16 21:34:34 +0300

    нормально переписал тесты

--- tests.cpp
+++ tests.cpp
@@ -1,48 +1,54 @@
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
 
 #include "tensor.hpp"
 #include "tensor_iterator.hpp"
 
-template <typename T>
-void addition(std::span<T*> ptrs) {
-    (*ptrs[2]) = (*ptrs[1]) + (*ptrs[0]);
-}
+using size_t = std::size_t;
 
 TEST_CASE("Tensor test") {
-    torch::Tensor<float> t({2, 3, 4});
+    torch::Tensor<int> t{{2, 3, 4}};
 
     REQUIRE(t.ndim() == 3);
     REQUIRE(t.size(1) == 3);
 }
 
 TEST_CASE("TensorIterator test") {
-    torch::Tensor<float> A{{2, 2}};
-    torch::Tensor<float> B{{2, 2}};
-    torch::Tensor<float> C{{1, 2}};
-
-    C[{0, 0}] = 5;
-    C[{0, 1}] = 6;
-
-    B[{0, 0}] = 1;
-    B[{0, 1}] = 2;
-    B[{1, 0}] = 3;
-    B[{1, 1}] = 4;
-
-    float* ptr = B.data_ptr();
-
-    for (size_t i = 0; i < 4; i++) {
-        REQUIRE((*(ptr + i) == (float)i + 1));
-    }
+    torch::Tensor<float> A{{2, 3}};
+    torch::Tensor<float> B{{2, 3}};
+    torch::Tensor<float> C{{1, 3}};
 
     torch::iterator::TensorIterator<float> iter{{&B, &C}, {&A}};
-    torch::shape_t shape{2, 2};
+    torch::shape_t shape{2, 3};
 
     REQUIRE((shape == iter.shape()));
 
-    iter.run(addition<float>);
+    auto srides_C = iter.compute_strides(C.strides(), C.shape());
+    auto srides_B = iter.compute_strides(B.strides(), B.shape());
 
-    CHECK((A[{0, 0}] == 6));
-    CHECK((A[{0, 1}] == 8));
-    CHECK((A[{1, 0}] == 8));
-    CHECK((A[{1, 1}] == 10));
+    REQUIRE(srides_C.size() == 2u);
+    REQUIRE(srides_B.size() == 2u);
+
+    REQUIRE(srides_C == torch::shape_t{0, 1});
+    REQUIRE(srides_B == torch::shape_t{3, 1});
+}
+
+TEST_CASE("Tensor operations test") {
+    torch::Tensor<int> t1{{2, 3, 4}};
+    torch::Tensor<int> t2{{2, 3, 4}};
+
+    for (size_t i = 0; i < t1.numel(); i++) {
+        t1[i] = t2[i] = (int)i;
+    }
+
+    torch::Tensor<int> res1 = t1 * t2;
+
+    for (size_t i = 0; i < t1.numel(); i++) {
+        CHECK(t1[i] * t2[i] == res1[i]);
+    }
+
+    torch::Tensor<int> res2 = t1 * 2;
+    for (size_t i = 0; i < t1.numel(); i++) {
+        CHECK(t1[i] * 2 == res2[i]);
+    }
 }
